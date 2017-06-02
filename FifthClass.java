91ca1e572045bcd5ea6ec4d3dd5fbeaaacb8fc71
kondratenko.i.b <mgirina89@gmail.com>
2017-06-02 12:23:25 +0300

    first version

---
+++ FifthClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by kondratenko.i.b on 02.06.2017.
+ */
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс Java");
+    }
+}
