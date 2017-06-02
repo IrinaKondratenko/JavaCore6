6984c01ac0c23421a99afe4e14c1726324c97b28
kondratenko.i.b <mgirina89@gmail.com>
2017-06-02 12:34:27 +0300

    third version

--- FirstClass.java
+++ FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Мой первый класс на Java");
         System.out.println("done....");
+        System.out.println("done....");
+        System.out.println("done....");
     }
 
 
